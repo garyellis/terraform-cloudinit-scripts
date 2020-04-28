04e90f3e9221ad8b4cad0a43777313d32c250f7d
gary ellis <gary.luis.ellis@gmail.com>
2020-04-28 09:56:22 -0700

    parameterize docker yum repo

--- variables.tf
+++ variables.tf
@@ -1,55 +1,67 @@
 variable "gzip" {
   description = "gzip the cloud-init config"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "base64_encode" {
   description = "base64 encode the cloud-init config"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "extra_user_data_script" {
   description = "append an extra userdata shell script to cloud-init"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "extra_user_data_yaml" {
   description = "add cloud-config userdata configuration"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "install_ansible" {
   description = "instal ansible"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "install_docker" {
   description = "install docker"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "install_docker_compose" {
   description = "install docker compose"
-  type = bool
-  default = false
+  type        = bool
+  default     = false
 }
 
 variable "docker_compose_version" {
   description = "the docker compose version"
-  type = string
-  default = "1.24.1"
+  type        = string
+  default     = "1.24.1"
 }
 
 variable "docker_version" {
   description = "the docker version to install"
-  type = string
-  default = "19.03.8"
+  type        = string
+  default     = "19.03.8"
+}
+
+variable "docker_yum_repo_url" {
+  description = "override the docker yum repo url as needed"
+  type        = string
+  default     = "https://download.docker.com/linux/centos/7/$basearch/stable"
+}
+
+variable "docker_yum_repo_gpgkey_url" {
+  description = "override the docker yum repo gpg key as needed"
+  type        = string
+  default     = "https://download.docker.com/linux/centos/gpg"
 }
 
 variable "install_http_proxy_env" {
